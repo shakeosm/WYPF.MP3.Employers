645e0a0c2d1f89ee3501ed0020ff21338614508b
Shawkat Osman <shawkat.osman@bradford.gov.uk>
2023-05-16 16:51:11 +0100

    removed comment

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,5 @@
 ﻿$(function () {
-    console.log("$(function () -site.js"); //TODO: Remove this line
-
+    
     //$(window).on("beforeunload", function () {
     //    console.log("gonna call API to log this user session out");
     //    return confirm("Please use 'Log out'  button next time for a secure logout?");
