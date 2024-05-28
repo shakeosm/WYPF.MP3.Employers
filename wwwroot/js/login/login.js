ca3d4d045a05a4b5a885ea1f1212680fb9848817
Shawkat Osman <shawkat.osman@bradford.gov.uk>
2024-05-28 11:14:32 +0100

    Login form refactor

--- wwwroot/js/login/login.js
+++ wwwroot/js/login/login.js
@@ -5,5 +5,15 @@
     $("#BrowserIdInput").val(instance.getBrowserId() + '-' + instance.getBrowserVersion());
     $("#WindowsIdInput").val( instance.getOsId() );
 
+    $("#SendResetRequestButton").click(function () {        
+        $("#PasswordRequestSentConfirmationDiv").removeClass("d-none");
+        $("#ForgottenPasswordRequestDiv").slideUp(500);
 
+    });
+
+
+    $("#ForgottenPasswordButton").click(function() {
+        $("#ForgottenPasswordDiv").removeClass("d-none");
+        $("#LoginForm").slideUp(500);
+    });
 });
